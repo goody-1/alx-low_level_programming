6784c5212308b2edb79936647795bb17a33afbca
goody-1 <atundeg@gmail.com>
2021-06-17 15:51:56 +0100

    updated the script, 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 
 /**
- * main function is just to print the text with new line
- * this is a part of the training at ALX
+ * main - is just to print the text with new line
+ * Description: this is a part of the training at ALX
+ * Return: return value is 0
  */
 
 int main(void)
