f8b4ef44c4cff9faa681f468c36f5fdff82f0c1e
goody-1 <atundeg@gmail.com>
2021-06-17 15:48:50 +0100

    updated the script, 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 
+/**
+ * main function is just to print the text with new line
+ * this is a part of the training at ALX
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
