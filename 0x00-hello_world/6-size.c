bf1650b4c49653fb77657420a5c527bf8ef55ae2
goody-1 <atundeg@gmail.com>
2021-06-17 16:45:46 +0100

    updated script, 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,11 @@
 
 int main(void)
 {
-	printf(sizeof(int));
-	printf(sizeof(float));
-	printf(sizeof(char));
-	printf(sizeof(double));
+	printf("Size of a char: %ld\n byte(s)", sizeof(char));
+	printf("Size of an int: %ld\n byte(s)", sizeof(int));
+	printf("Size of a long int: %ld\n byte(s)", sizeof(long int));
+	printf("Size of a long long int: %ld\n byte(s)", sizeof(long long int));
+	printf("Size of a float: %ld\n byte(s)", sizeof(float));
 
 	return (0);
 }
