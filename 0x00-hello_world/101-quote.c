cc216bea63aefafe95651224b806facaa9b26470
goody-1 <atundeg@gmail.com>
2021-06-17 21:49:29 +0100

    added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - print a text
+ * Description: nothing much, just print a text
+ * Return: return value is 0
+ */
+
+int main(void)
+{
+	printf("");
+
+	return (0);
+}
