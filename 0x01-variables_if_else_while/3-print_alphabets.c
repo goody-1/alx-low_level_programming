f6ec49e7834347ae564ebdea49f6ee3cb0040b31
goody-1 <atundeg@gmail.com>
2021-06-18 21:04:15 +0100

    updated 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,19 +11,19 @@
 int main(void)
 {
 	char ch;
-	
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
-	
+
 	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch);
 	}
-	
+
 	putchar('\n');
-	
+
 	return (0);
 
 }
