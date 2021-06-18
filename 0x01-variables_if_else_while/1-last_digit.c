c08bf49ebd2319db858877e8a3a4ae0cd81a7c40
goody-1 <atundeg@gmail.com>
2021-06-18 19:59:39 +0100

    updated script, 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,9 +6,9 @@
 /* betty style doc for function main goes there */
 /**
  * main - print last digit of random number
- * Description: print last digit of random nubmer and 
+ * Description: print last digit of random nubmer and
  * tell if it is greater than 5 or is 0 or is less than 6 and is not 0
- * 
+ *
  * Return: return value is 0
  */
 
@@ -23,7 +23,7 @@ int main(void)
 	/* your code goes there */
 	digit = abs(n)%10;
 
-	if (n < 0) 
+	if (n < 0)
 	{
 		digit = -digit;
 	}
@@ -39,22 +39,6 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, digit);
 	}
-/*	else if (n < 0)
-	{	
-		if (digit > 5)
-		{
-			printf("Last digit of %d is %d and is greater than 5\n", n, -digit);
-		}
-		else if (digit < 6 && digit != 0)
-		{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, -digit);
-		}
-		else if (digit == 0)
-		{
-			printf("Last digit of %d is %d and is 0\n", n, digit);
-		}
-	}
-*/
 
 	return (0);
 }
