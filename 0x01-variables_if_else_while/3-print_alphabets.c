423eb7f0b51b0112e93a2835822cc6d6e8caa32f
goody-1 <atundeg@gmail.com>
2021-06-18 21:01:59 +0100

    updated 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,25 +5,25 @@
  * main - print lowercase characters of the english alphabet
  *
  * Return: return value
- *     
+ *
  */
 
 int main(void)
 {
-        char ch;
-
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-                putchar(ch);
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
 	}
-
+	
 	for (ch = 'A'; ch <= 'Z'; ch++)
-        {
-                putchar(ch);
+	{
+		putchar(ch);
 	}
-        
+	
 	putchar('\n');
-
+	
 	return (0);
 
 }
