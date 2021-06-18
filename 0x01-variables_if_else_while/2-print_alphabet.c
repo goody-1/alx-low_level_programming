219db732aae23ffc3fbfad87e7f411ea79478167
goody-1 <atundeg@gmail.com>
2021-06-18 20:21:29 +0100

    added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print lowercase characters of the english alphabet
+ *
+ * Return: return value 
+ */
+
+int main(void)
+{
+	char ch = '97';
+
+	for (ch = '97'; ch <= '122'; ch++)
+		        putchar(ch);
+	return (0);
+
+}
