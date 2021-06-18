e3544bb9c274af79e4f2e2712aa7ea5e298297fc
goody-1 <atundeg@gmail.com>
2021-06-18 18:37:25 +0100

    updated script, 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,15 +23,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0) 
 	{
-		printf("%ld is positive");
+		printf("%ld is positive", n);
 	}
 	else if (n == 0)
 	{
-		print("%ld is zero");	
+		print("%ld is zero", n);	
 	}
 	else
 	{
-		print("%ld is negative");
+		print("%ld is negative", n);
 	}
 	return (0);
 }
