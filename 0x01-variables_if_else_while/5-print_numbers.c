d0273a300e05bcbfe293774db33e0db46673f3db
goody-1 <atundeg@gmail.com>
2021-06-18 21:18:18 +0100

    added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *  * main - print lowercase characters of the english alphabet
+ *   *
+ *    * Return: return value
+ *     */
+
+int main(void)
+{
+	char ch;
+	
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	
+	putchar('\n');
+	
+	return (0);
+
+}
