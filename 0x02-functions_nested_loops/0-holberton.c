4594e2f421aa68e1f204448f3140e60392e7c419
Goodness <atundeg@gmail.com>
2021-06-23 02:15:28 +0100

    added 0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - the main program
+ *
+ * Return: returns 0
+ *
+ */
+
+int main(void)
+{
+	char text = "Holberton";
+
+	for(int index = '0'; index <='9'; index++ )
+	{
+		_putchar(text[index]);
+	}
+	_putchar('\n');
+
+	return 0
+}
