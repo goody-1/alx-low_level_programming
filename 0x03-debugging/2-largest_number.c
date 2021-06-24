fa1d18330d0555a77b18592c8059c53d30bed159
Goodness <atundeg@gmail.com>
2021-06-24 21:25:04 +0100

    updates

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -28,6 +28,14 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
+	else if (c > a && a >= b)
+	{
+		largest = c;
+	}
+	else if (c > b && b >= a)
+	{
+		largest = c;
+	}
 	else
 	{
 		largest = c;
