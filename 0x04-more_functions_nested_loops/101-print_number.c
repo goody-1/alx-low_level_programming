e227c18379ed920f92ca016d9759ba8f0f146fe6
Goodness <atundeg@gmail.com>
2021-06-25 19:02:46 +0100

    added 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+* print_number - prints an integer
+* @n: integer to be printed
+* Return: no return (return is void)
+*/
+
+void print_number(int n)
+{
+	char c;
+
+	if (n >= 0 && n =< 9)
+	{
+		_putchar('0' + n);
+	}
+	else if (n < 0)
+	{
+		n = -n;
+
+		if (n >= 0 && n <= 9)
+		{
+			_putchar('-');
+			_putchar('0' + n);
+		}
+		else
+		{
+			c = n % 10;
+			_putchar('-');
+			_putchar('1');
+			_putchar('0' + c);
+		}
+	}
+	else
+	{
+		c = n % 10;
+		_putchar('1');
+		_putchar('0' + c);
+	}
+
+}
+
