4105f12950ab2db781e6a5f7f7faab2b02be9c04
Goodness <atundeg@gmail.com>
2021-06-25 10:57:02 +0100

    added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* print_numbers - prints numbers 0 to 9 followed by new line
+* 
+* Return: no return (return is void)
+*/
+
+void print_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+
+	_putchar('\n');
+
+	return;
+}
+
