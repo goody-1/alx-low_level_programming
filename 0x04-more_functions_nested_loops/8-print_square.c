004f09cea0d23101ad7b2eb714ec1f28fc565833
Goodness <atundeg@gmail.com>
2021-06-25 14:30:06 +0100

    added the file 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+* print_most_numbers - prints numbers 0 to 9
+* @size: size of square
+* Return: no return (return is void)
+*/
+
+void print_square(int size)
+{
+	int count, n;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (count = 1; count <= size; count++)
+		{
+			for (n = 1; n <= size; n++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+
+}
+
