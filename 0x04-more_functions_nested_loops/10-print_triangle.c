abbf95f2929bee1e29b74c5dfbe266b4b7b7485f
Goodness <atundeg@gmail.com>
2021-06-25 15:51:53 +0100

    edited 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,19 +16,19 @@ void print_triangle(int size)
 	}
 	else
 	{
-	for (count = 1; count <= size; count++)
+		for (count = 1; count <= size; count++)
 		{
 			for (n = size - 1; n >= count; n--)
 			{
 				_putchar(' ');
 			}
-            for (n = 1; n <= count; n++)
-            {
-			_putchar('#');
-            }
-
+			for (n = 1; n <= count; n++)
+			{
+				_putchar('#');
+			}
 			_putchar('\n');
 		}
 	}
 
 }
+
