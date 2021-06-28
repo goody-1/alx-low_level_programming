3cc410be629362047a49e6ba05528d938ce35333
Goodness <atundeg@gmail.com>
2021-06-28 18:36:18 +0100

    attempted task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+* print_array - prints n elements of an array of integers,
+* followed by a new line.
+* @a: pointer
+* @n: number of times to print
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar(a[i]);
+		_putchar(',');
+		_putchar(' ');
+	}
+
+	_putchar('\n');
+}
+
