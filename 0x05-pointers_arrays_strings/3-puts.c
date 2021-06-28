0790d1bc945b6e7c3f3230159d484fcc22d5b754
Goodness <atundeg@gmail.com>
2021-06-28 12:49:45 +0100

    did task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* _puts - prints a string, followed by a new line,
+* to stdout
+* @str: pointer
+* Return: void
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
+
