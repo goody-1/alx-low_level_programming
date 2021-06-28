486999dad81b8a1982b82a725da39c0160ca1141
Goodness <atundeg@gmail.com>
2021-06-28 13:08:52 +0100

    did task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* print_rev - prints a string, in reverse,
+* followed by a new line.
+* @s: pointer
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0') 
+	{
+		i++;
+	}
+
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
+
