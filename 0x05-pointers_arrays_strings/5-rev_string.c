08d5ba38c32df3030dbcbf36c4f933705124e6ec
Goodness <atundeg@gmail.com>
2021-06-29 21:33:43 +0100

    corrected rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,9 +9,9 @@
 void rev_string(char *s)
 {
 	int i, j, len_of_str;
-    char *begin_ptr, *end_ptr, ch;
+	char *begin_ptr, *end_ptr, ch;
 
-    i = 0;
+	i = 0;
 
 	while (s[i] != '\0')
 	{
@@ -19,30 +19,30 @@ void rev_string(char *s)
 	}
 
 	len_of_str = i  - 1;
-    begin_ptr = s;
-    end_ptr = s;
-    
-    /* Move the end_ptr to the last character */
+	begin_ptr = s;
+	end_ptr = s;
 
-    for (j = 0; j < len_of_str; j++)
-    {
-        end_ptr++;
-    }
+	/* Move the end_ptr to the last character */
 
-    /* Swap the char from start and end */
-    /* index using begin_ptr and end_ptr */
+	for (j = 0; j < len_of_str; j++)
+	{
+		end_ptr++;
+	}
+
+	/* Swap the char from start and end */
+	/* index using begin_ptr and end_ptr */
 
 	for (j = 0; j < len_of_str / 2; j++)
 	{
-        /* swap character */
-        ch = *end_ptr;
-        *end_ptr = *begin_ptr;
-        *begin_ptr = ch;
+		/* swap character */
+		ch = *end_ptr;
+		*end_ptr = *begin_ptr;
+		*begin_ptr = ch;
 
-        /* update pointers positions */
-        begin_ptr++;
-        end_ptr--;
+		/* update pointers positions */
+		begin_ptr++;
+		end_ptr--;
 	}
 
-	putchar('\n');
 }
+
