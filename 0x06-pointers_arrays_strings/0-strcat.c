0f6323067ba2c16ab165f37fe139b4ec285595be
Goody <atundeg@gmail.com>
2021-07-01 00:19:40 +0100

    fixed some requirements

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,13 +7,16 @@
 * overwriting the terminating null byte (\0) at the end of dest,
 * and then adds a terminating null byte
 *
+* @dest: this is the destination of the result
+* @src: this is the string to be added to the dest
+*
 * Return: a pointer to the resulting string dest;
 */
 
 
 char *_strcat(char *dest, char *src)
 {
-	int i,j,m;
+	int i, j, m;
 
 	while (dest[i] != '\0')
 	{
