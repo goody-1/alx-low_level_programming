1e46b9197ae4d4e0df1e8f9bba4005e5b4eec20e
Goody <atundeg@gmail.com>
2021-07-01 00:46:25 +0100

    attempted task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+* _strncat - concatenates two strings
+*
+* Description: This function appends the src string to the dest string,
+* it will use at most n bytes from src; and src does not need to be
+* null-terminated if it contains n or more bytes
+*
+* @dest: this is the destination of the result
+* @src: this is the string to be added to the dest
+*
+* Return: a pointer to the resulting string dest;
+*/
+
+
+char *_strncat(char *dest, char *src)
+{
+	int i = 0, j = 0, m;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+
+	for (m = 0; m < j; m++)
+	{
+		dest[i] = src[m];
+		i++;
+	}
+
+	return (dest);
+}
