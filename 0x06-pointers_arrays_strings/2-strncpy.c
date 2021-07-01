6b773310c70827a6c80626177511684f45272e6c
Goody <atundeg@gmail.com>
2021-07-01 21:21:07 +0100

    did task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* _strncpy - copies a string
+*
+* Description: should work exactly like strncpy
+*
+* @dest: this is the destination of the result
+* @src: this is the string to be copied to the dest
+* @n: maximum number of bytes to be used from src
+*
+* Return: a pointer to the resulting string dest;
+*/
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, m;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+
+	}
+
+	return (dest);
+}
