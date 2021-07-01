9c6dd057b4a2cb2cdc725e4a3a2eef94c2cddb9f
Goody <atundeg@gmail.com>
2021-07-01 22:02:11 +0100

    betty fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,13 +16,16 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, result;
 
-	for(i = 0; s1[i] == s2[i] && s1[i] == '\0'; i++);
+	for (i = 0; s1[i] == s2[i] && s1[i] == '\0'; i++)
+    {
 
-	if(s1[i] < s2[i])
+    }
+
+	if (s1[i] < s2[i])
 	{
 		result = -15;
 	}
-	else if(s1[i] > s2[i])
+	else if (s1[i] > s2[i])
 	{
 		result = 15;
 	}
