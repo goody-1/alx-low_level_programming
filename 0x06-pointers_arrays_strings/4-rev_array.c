d833398905c616548973ab096fc92a0ca6ee99c0
Goodness <atundeg@gmail.com>
2021-07-02 14:52:29 +0100

    did task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+* reverse_array - reverses the content of an array of integers
+*
+* @a: array
+* @n: number of elements of the array
+*
+* Return: void
+*/
+
+
+void reverse_array(int *a, int n)
+{
+	int s[n];
+	int i=0, c, j=0;
+
+	for (c = n - 1; c >= 0; c--)
+	{
+		s[i] = a[c];
+
+		i++;
+	}
+
+	/* a = s; */
+
+	while (j < n)
+	{
+		*(a + j) = *(s + j);
+
+		j++;
+	}
+
+	/* *a = *s; */
+
+	/* while (j < n)
+	 * {
+	 * printf("%d ", a[j]);
+	 * j++;
+	 * }
+	 * */
+
+}
+
