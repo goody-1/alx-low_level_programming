a6071eac6fb2b4d4148d9fe66a67b3eaf55b3c4e
Goodness <atundeg@gmail.com>
2021-07-02 19:23:22 +0100

    did task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+* leet - encodes a string into 1337 (leet)
+* @str: the string to be converted
+* Return: a string of leeted words
+*/
+
+
+char *leet(char *str)
+{
+	int i = 0, j = 0;
+	char character[5] = {'a', 'e', 'o', 't', 'l'};
+	char leet[5] = {'4', '3', '0', '7', '1'};
+
+	while (str[i] != '\0')
+	{
+
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == character[j] || str[i] == character[j] - 32)
+			{
+				str[i] = leet[j];
+				break;
+			}
+
+		}
+		i++;
+	}
+
+	return (str);
+
+}
+
