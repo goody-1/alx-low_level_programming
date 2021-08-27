fd4aa7cbfb9809e7d42aae94e523308d8c480bf5
Goodness <atundeg@gmail.com>
2021-08-27 13:27:11 +0100

    added first advanced task, possible combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits.
+ *
+ * Return: returns void
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = '0'; num1 <= '9'; num1++)
+	{
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			if (num2 > num1)
+			{
+				putchar(num1);
+				putchar(num2);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
