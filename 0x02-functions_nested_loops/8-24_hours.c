79c7de919f13194dfcf3b33c33e68dfba8d9911c
Goodness <atundeg@gmail.com>
2021-08-31 19:30:31 +0100

    description of parameters - betty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,7 +3,8 @@
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer,
  * starting from 00:00 to 23:59
- *
+ * @h: hour integer
+ * @m: minute integer
  * Return: returns value of last digit
 **/
 
