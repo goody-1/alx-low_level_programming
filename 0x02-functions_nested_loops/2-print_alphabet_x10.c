16bfd79036f370525be2190e3671b133d443a52b
Goodness <atundeg@gmail.com>
2021-08-31 13:45:11 +0100

    task 2, print the alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase characters of the alphabet
+ *
+ * Return: Always 0.
+**/
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+
+		_putchar('\n');
+	}
+
+}
