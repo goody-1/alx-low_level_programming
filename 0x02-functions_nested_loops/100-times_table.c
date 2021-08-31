70911c1c4c3cd35c4c32e30e59aebfe9c4f859d3
Goodness <atundeg@gmail.com>
2021-08-31 21:49:40 +0100

    advanced task 1, prints n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: limit of times table
+ * Return: returns void
+**/
+
+
+void print_times_table(int n)
+{
+	int i, j, res;
+
+	if (n > 0 && n < 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				res = i * j;
+				my_putchar(res);
+
+				if (j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (res < 10)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
+
+/**
+ * my_putchar - prints numbers as they are with putchar
+ * @var: number to be tested
+ * Return: returns no value
+**/
+
+void my_putchar(int var)
+{
+
+	/* print '-' for negative numbers */
+
+	if (var < 0)
+	{
+		_putchar('-');
+		var = var * -1;
+	}
+
+	/*
+	 * First remove the last digit of number and print
+	 * the remaining digits using recursion, then print
+	 * the last digit
+	*/
+
+	if (var / 10)
+		my_putchar(var / 10);
+
+	_putchar(var % 10 + '0');
+}
