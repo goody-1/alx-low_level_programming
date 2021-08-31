5f4fc1757ba599f03b887efe575d8b8b560c9d3d
Goodness <atundeg@gmail.com>
2021-08-31 22:03:12 +0100

    advanced task 2,

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - main program
+ * Return: returns 0
+**/
+
+
+int main(void)
+{
+	int i, res = 1;
+
+	for (i = 3; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			res += i;
+		}
+	}
+	printf("%d\n", res);
+
+	return (0);
+}
