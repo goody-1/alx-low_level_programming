6092641c7d8aa540fe1bd34f2e7342387cf849e9
Goodness <atundeg@gmail.com>
2021-09-01 16:58:20 +0100

    advanced task 3, fibonacci, overflow?

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - main program
+ * Return: returns 0
+**/
+
+int main(void)
+{
+	int i;
+	unsigned int fib[49];
+
+	fib[0] = 1; fib[1] = 2;
+
+	printf("%d, %d, ", fib[0], fib[1]);
+
+	for (i = 2; i < 50; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+		if (i == 49)
+		{
+			printf("%lu", fib[i]);
+		}
+		else
+		{
+			printf("%lu, ", fib[i]);
+		}
+	}
+
+	return (0);
+}
