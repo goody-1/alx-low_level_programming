a98b5ef64665742c9102af57c9144dd41b09e2a2
Goodness <atundeg@gmail.com>
2021-09-15 16:28:57 +0100

    removed static variables

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,13 +10,13 @@
 
 int _strlen_recursion(char *s)
 {
-	static int i = 0;
-
-	if (*s)
+	if (!*s)
 	{
-		_strlen_recursion(s + 1);
-		i++;
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
 	}
 
-	return (i);
 }
