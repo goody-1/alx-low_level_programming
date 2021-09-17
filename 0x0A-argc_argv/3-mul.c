aaa8bc858b6d563e4b62b3092c34929826dbd89c
Goodness <atundeg@gmail.com>
2021-09-17 21:04:33 +0100

    task 3, multiply just two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+* main - a program that multiplies two numbers
+*
+* @argc: argument count
+*
+* @argv: argument vector
+*
+* Return: always 0
+*/
+
+int main(int argc, int *argv[])
+{
+
+	if (argc < 2 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", argv[0] * argv[1]);
+
+	return (0);
+}
