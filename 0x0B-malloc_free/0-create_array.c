2e8a81010b6f4e407ae850a298495a29275ead2d
Goodness <atundeg@gmail.com>
2021-09-23 12:01:10 +0100

    correct comparison

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 
     ar = malloc(size * sizeof(char));
 
-    if (ar == NULL || size = 0)
+    if (ar == NULL || size == 0)
         return (NULL);
     else
     {
