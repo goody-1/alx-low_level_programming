48e936bc6f2a7a9c0a3adb27bac7e06882277e9b
Goodness <atundeg@gmail.com>
2021-09-23 12:22:39 +0100

    betty style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,8 @@
 
 
 /**
- * create_array - a function that creates an array of chars, and initializes it with a specific char
+ * create_array - a function that creates an array of chars,
+ * and initializes it with a specific char
  *
  * @c: initializer
  *
@@ -20,11 +21,9 @@ char *create_array(unsigned int size, char c)
 
 	if (ar == NULL || size == 0)
 		return (NULL);
-	else
-	{
-		for (i = 0; i < size; i++)
+
+	for (i = 0; i < size; i++)
 		ar[i] = c;
-	}
 
 	return (ar);
 }
