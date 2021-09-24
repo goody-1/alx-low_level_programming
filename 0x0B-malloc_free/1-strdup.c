e2b0c195ec8215ddff6b3f5f8c32514825b69485
Goodness <atundeg@gmail.com>
2021-09-24 03:31:11 +0100

    handling segfaults

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,19 +17,19 @@ char *_strdup(char *str)
 	char *s;
 	int i, len_str = 0;
 
+	if (str == NULL)
+		return (NULL);
+
 	while (str[len_str] != '\0')
 		len_str++;
 
 	s = malloc((len_str + 1) * sizeof(char));
 
-	if (str == NULL)
+	if (s == NULL)
 		return (NULL);
 
 	for (i = 0; i < len_str; i++)
 		s[i] = str[i];
-
-	if (s == NULL)
-		return (NULL);
 
 	return (s);
 }
