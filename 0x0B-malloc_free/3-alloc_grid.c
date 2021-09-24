275861e323570071f3d3e45d628127e70de0a456
Goodness <atundeg@gmail.com>
2021-09-24 04:47:29 +0100

    handling malloc return

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,10 +21,10 @@ int **alloc_grid(int width, int height)
 {
 	int i, j;
 
+	int **ar = (int **)malloc(height * sizeof(int *));
+
 	if (width < 1 || height < 1)
 		return (NULL);
-
-	int **ar = (int **)malloc(height * sizeof(int *));
 
 	for (i = 0; i < height; i++)
 		ar[i] = (int *)malloc(width * sizeof(int));
