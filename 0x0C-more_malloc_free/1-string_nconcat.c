65dfc3d2ad85ff8c958402a97d424440a8ca5533
Goodness <atundeg@gmail.com>
2021-09-25 23:21:09 +0100

    task 1 doing, string concat but certain num of bytes from s2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+
+/**
+ * string_nconcat - function that concatenates two strings
+ *
+ * Description: The returned pointer shall point to a newly allocated space
+ * in memory, which contains s1, followed by
+ * the first n bytes of s2, and null terminated
+ * If the function fails, it should return NULL
+ * If n is greater or equal to the length of s2 then use the entire string s2
+ * If NULL is passed, treat it as an empty string
+ *
+ * @s1: first string to be concatenated
+ * @s2: second string to be concatenated
+ * @n: number of bytes of s2 to be added
+ *
+ * Return: pointer to a newly allocated space in memory which contains the
+ * contents of s1, followed by the contents of s2, and null terminated
+ * NULL is returned on failure
+ *
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	char *s;
+	unsigned int i, j, len_s1 = 0, len_s2 = 0, len_s;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len_s1] != '\0')
+		len_s1++;
+
+	while (s2[len_s2] != '\0')
+		len_s2++;
+
+    if (n >= len_s2)
+		n = len_s2;
+
+	len_s = len_s1 + n;
+
+	s = malloc((len_s + 1) * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < len_s1; i++)
+		s[i] = s1[i];
+
+
+	for (j = 0; j < n; j++)
+	{
+		s[len_s1] = s2[j];
+		len_s1++;
+	}
+
+	return (s);
+}
