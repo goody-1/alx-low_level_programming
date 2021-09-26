413b776dc8108c4547c26743a15ec98244333620
Goodness <atundeg@gmail.com>
2021-09-26 16:23:45 +0100

    task 2, building calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+/**
+ * _calloc - function that allocates memory for an array, using malloc
+ *
+ * Description: The _calloc function allocates memory for an array of
+ * nmemb elements of size bytes each and returns a pointer to the
+ * allocated memory.
+ * The memory is set to zero
+ * If nmemb or size is 0, then _calloc returns NULL
+ *
+ * @nmemb: first string to be concatenated
+ * @size: second string to be concatenated
+ *
+ * Return: If malloc fails, then _calloc returns NULL
+ *
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *s;
+	int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	s = malloc(nmemb * size);
+
+	if (s == NULL)
+		return (NULL);
+
+	while (i < size)
+		s[i] = 0;
+
+	return (s);
+}
