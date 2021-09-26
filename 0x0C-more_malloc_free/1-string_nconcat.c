e8ca5eb5cf82772e362c0ad8ae406f7e34f1d5b8
Goodness <atundeg@gmail.com>
2021-09-26 15:48:39 +0100

    attempt to fix all issues

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,6 +38,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[len_s2] != '\0')
 		len_s2++;
 
+	if (n >= len_s2)
+		n = len_s2;
 
 	len_s = len_s1 + n;
 
@@ -45,9 +47,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s == NULL)
 		return (NULL);
-
-	if (n >= len_s2)
-		n = len_s2;
 
 	for (i = 0; i < len_s1; i++)
 		s[i] = s1[i];
