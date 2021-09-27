aa666fa22c4479df7a87457cfa39288eac095da2
Goodness <atundeg@gmail.com>
2021-09-27 19:10:48 +0100

    satisfied betty style and documentation for dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,19 @@
 #ifndef DOG_H
+#define DOG_H
 
-typedef struct dog {
-    char *name;
-    char *owner;
-    float age;
+/**
+ * struct dog - definition dog structure
+ * @name: parameter for dog name
+ * @owner: parameter for owner name
+ * @age: parameter for dog age
+ *
+ */
+
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
 } dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
