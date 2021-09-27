7b2fd7afab95e57011529b76d8d0eedc2018c11c
Goodness <atundeg@gmail.com>
2021-09-27 20:45:20 +0100

    betty fixes for header files and all

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,12 @@
 #ifndef ABS
+#define ABS
+
+/**
+ * ABS - funtion-like macro to get absolute value of number
+ *
+ * @x: the number to be processed
+ *
+ */
 
 #define ABS(x) ((x < 0) ? -x : x)
 
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,4 +1,11 @@
 #ifndef SUM
+#define SUM
+
+/**
+ *	SUM - the sum function-like macro
+ * @x: first num to be added
+ * @y: second num to be added
+ */
 
 #define SUM(x, y) ((x) + (y))
 
