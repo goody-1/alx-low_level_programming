d19fda10bda5232e3ccf5b51dcf3a740b29beb28
Goodness <atundeg@gmail.com>
2021-09-27 19:36:57 +0100

    responding appropriately to all instructions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,8 +15,20 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		/* check if d.name or d.owner is null and respond accordingly */
+
+		if (d->name == NULL)
+			d->name = "(nil)";
+		else if (d->owner == NULL)
+			d->owner = "(nil)";
+
+		/* handling d.age of type float separately */
+
+		if (d->age)
+		{
+			printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		}
+		else
+			printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
 	}
-	else
-		printf("");
 }
