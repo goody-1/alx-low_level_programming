0ffff3a9162d03146517bf1edf7641168845d246
Goodness <atundeg@gmail.com>
2021-09-27 13:19:53 +0100

    tasks 1 and 2, macros and __FILE__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - main program, prints name of file compiled from, followed by new line
+*
+* Return: always 0
+*
+*/
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+
+	return (0);
+}
