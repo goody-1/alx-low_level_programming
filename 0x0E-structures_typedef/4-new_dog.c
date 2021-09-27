4b5270c07277c0c8a9ce4c743fb3193ba0071515
Goodness <atundeg@gmail.com>
2021-09-27 20:28:43 +0100

    another try

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,14 +18,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = malloc(sizeof(*new_dog));
 
-	new_dog->name = name;
+	if (new_dog == NULL)
+		return (NULL);
 
+	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
 
-	if (new_dog != NULL)
-		return (new_dog);
-	else
-		return (NULL);
+	return (new_dog);
 
 }
