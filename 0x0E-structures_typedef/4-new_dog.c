5e889611e7d86a096f81d31dea560dfff50363b4
Goody <atundeg@gmail.com>
2021-09-28 18:33:50 +0100

    review fixes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
 /**
  * new_dog - a function that prints creates a new dog
  *
@@ -16,20 +19,68 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *n;
-	char *o;
 	dog_t *new_dog = malloc(sizeof(*new_dog));
 
-	n = name;
-	o = owner;
-
 	if (new_dog == NULL)
+	{
+		free(new_dog);
 		return (NULL);
+	}
 
-	new_dog->name = n;
+	new_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+
+	if (new_dog->name == NULL)
+	{
+		free(new_dog->name);
+		return (NULL);
+	}
+	
+	if (new_dog->name == NULL)
+	{
+		free(new_dog->owner);
+		return (NULL);
+	}
+
+	new_dog->name = _strcpy(new_dog->name, name);
 	new_dog->age = age;
-	new_dog->owner = o;
+	new_dog->owner = _strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 
+}
+
+
+/**
+* _strcpy - copies the string pointed to by src,
+* including the terminating null byte (\0),
+* to the buffer pointed to by dest
+* @src: source pointer
+* @dest: destination pointer
+* Return: char
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+
+	return (dest);
+}
+
+/**
+* _strlen - returns the length of a string
+* @s: pointer
+* Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
 }
