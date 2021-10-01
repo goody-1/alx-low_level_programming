6fb167f8b5c24ad8912e3729a9e66f3450f1d26b
Goodness <atundeg@gmail.com>
2021-10-01 13:13:14 +0100

    corrected task 0 of project

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+void name(char *name);
+
+
+/**
+ * print_name - a function that prints a name
+ *
+ * @name: name to be printed
+ *
+ * Return: void
+ *
+ */
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
