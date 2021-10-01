035d6c7af70056ead9565a2ada046c4004bc0ccc
Goodness <atundeg@gmail.com>
2021-10-01 13:23:51 +0100

    handling NULL parameter to print_name function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,10 @@
 #include "function_pointers.h"
 
-void name(char *name);
-
-
 /**
  * print_name - a function that prints a name
  *
  * @name: name to be printed
- *@f: function pointer
+ * @f: function pointer
  *
  * Return: void
  *
@@ -16,5 +13,8 @@ void name(char *name);
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		return;
+
 	f(name);
 }
