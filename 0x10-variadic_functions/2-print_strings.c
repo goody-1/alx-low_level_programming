3515ec51336563e99e0129ce98488648f7046a80
Goodness <atundeg@gmail.com>
2021-10-03 21:17:48 +0100

    made a copy to va_arg

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,14 +16,17 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-
 	va_list string;
+	char *str;
 
 	va_start(string, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(string, char *));
+		str = va_arg(string, char *);
+
+		if (str != NULL)
+			printf("%s", str);
 
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
