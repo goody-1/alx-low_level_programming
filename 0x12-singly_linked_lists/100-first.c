9b7be6657663605d6ec3b1e8e1aad26097e8d1d2
Goodness <atundeg@gmail.com>
2021-10-08 12:00:50 +0100

    execute function before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * before_main - function to be executed before the main program
+ *
+ * Return: void
+ */
+
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -27,4 +27,7 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
+/* Advanced tasks */
+void before_main(void) __attribute__ ((constructor));
+
 #endif /* LIST_H */
