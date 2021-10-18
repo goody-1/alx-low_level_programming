8875e7fb394e2c4bbb8f7e857a3bbceb9f0be69a
Goodness <atundeg@gmail.com>
2021-10-18 17:38:36 +0100

    statisfied betty spacing

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0, sz;
 
-	fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if ((filename == NULL) || fd < 0)
 		return (-1);
