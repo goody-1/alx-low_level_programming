74d0f27b175d3602c392591b0a51603ea4c47165
Goodness <atundeg@gmail.com>
2021-10-19 15:11:31 +0100

    handled NULL text content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,15 +23,19 @@ int create_file(const char *filename, char *text_content)
 	if ((filename == NULL) || fd < 0)
 		return (-1);
 
-	while (text_content[len] != '\0')
-		len++;
+	/* necessary to check text content to avoid seg faults */
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
 
-	sz = write(fd, text_content, len);
+		sz = write(fd, text_content, len);
 
-	close(fd);
+		close(fd);
 
-	if (sz < 0)
-		return (-1);
+		if (sz < 0)
+			return (-1);
+	}
 
 	return (1);
 }
