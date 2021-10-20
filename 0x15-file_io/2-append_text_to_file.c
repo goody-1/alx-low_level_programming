1777d7dcab018da04e1a120ca96ea90380f02c46
Goodness <atundeg@gmail.com>
2021-10-20 20:43:11 +0100

    a lttle tweak and DRY

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,11 +27,12 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 
 		sz = write(fd, text_content, len);
-		close(fd);
 
 		if (sz < 1)
 			return (-1);
 	}
+
+	close(fd);
 
 	return (1);
 }
