bc0602e26e0c816a34e200843340a919c10d432f
Goodness <atundeg@gmail.com>
2021-12-03 09:35:34 +0100

    advanced task - Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - function to add 2 numbers
+ * @a: first number to add
+ * @b: second number to add
+ *
+ * Return: the sum of the numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function to subtract 2 numbers
+ * @a: minuend
+ * @b: subtraend
+ *
+ * Return: the difference of the numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function to multiply 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the product of the numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function for division
+ * @a: dividend
+ * @b: divisor
+ *
+ * Return: the quotient
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function for modulos
+ * @a: dividend
+ * @b: divisor
+ *
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
