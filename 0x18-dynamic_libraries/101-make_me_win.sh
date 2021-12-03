706ebfac62007d2ca388a52e9fbd70b4aa00ed20
Goodness <atundeg@gmail.com>
2021-12-03 12:47:23 +0100

    new commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/goody-1/alx-low_level_programming/blob/main/0x18-dynamic_libraries/hijack.so
+wget -O /tmp https://github.com/goody-1/alx-low_level_programming/blob/main/0x18-dynamic_libraries/hijack.so
 export LD_PRELOAD=/tmp/hijack.so
