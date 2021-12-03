27c94107cbb5e32166f86036faeda4a0c9d3f7cb
Goodness <atundeg@gmail.com>
2021-12-03 13:16:57 +0100

    changed shared library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/hijack.so https://github.com/goody-1/alx-low_level_programming/blob/main/0x18-dynamic_libraries/hijack.so
-export LD_PRELOAD=/tmp/hijack.so
+wget -O /tmp/libwin.so https://github.com/goody-1/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libwin.so
+export LD_PRELOAD=/tmp/libwin.so
