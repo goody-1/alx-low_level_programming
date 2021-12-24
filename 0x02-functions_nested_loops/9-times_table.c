14ee2b261d9b5ea5c060d58c5eb924f0e7a8d995
Goodness <atundeg@gmail.com>
2021-12-24 02:21:01 +0100

    betty linter fixes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,23 +10,23 @@ void times_table(void)
 	int i, j, p;
 
 	for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            p = i * j;
-            if (j != 0 && p / 10 == 0)
-                _putchar(' ');
-            else if (j != 0)
-                _putchar(p / 10 + '0');
-            _putchar(p % 10 + '0');
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			p = i * j;
+			if (j != 0 && p / 10 == 0)
+				_putchar(' ');
+			else if (j != 0)
+				_putchar(p / 10 + '0');
+			_putchar(p % 10 + '0');
 
-            if (j != 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-            else
-                _putchar('\n');
-        }
-    }
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				_putchar('\n');
+		}
+	}
 }
