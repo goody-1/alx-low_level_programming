3935a6856f2b7a847e7dfa99dcbddd4bd3b8c0d5
Goodness <atundeg@gmail.com>
2021-12-27 11:49:44 +0100

    converted indentation to tabs

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,22 @@
 
 void rev_string(char *s)
 {
-    int i = 0, len = 0;
-    char temp;
+	int i = 0, len = 0;
+	char temp;
 
-    while (s[len] != '\0')
-    {
-        len++;
-    }
-    len--;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	len--;
 
-    while (i <= len)
-    {
-        temp = s[i];
-        s[i] = s[len];
-        s[len] = temp;
-        i++;
-        len--;
-    }
+	while (i <= len)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+		i++;
+		len--;
+	}
 
 }
