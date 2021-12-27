3351f0a1aa98f6c75cdf2b193b459bcf1d860678
Goodness <atundeg@gmail.com>
2021-12-27 11:38:25 +0100

    revisited code, made simpler

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,41 +8,22 @@
 
 void rev_string(char *s)
 {
-	int i, j, len_of_str;
-	char *begin_ptr, *end_ptr, ch;
+    int i = 0, len = 0;
+    char temp;
 
-	i = 0;
+    while (s[len] != '\0')
+    {
+        len++;
+    }
+    len--;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	len_of_str = i  - 1;
-	begin_ptr = s;
-	end_ptr = s;
-
-	/* Move the end_ptr to the last character */
-
-	for (j = 0; j < len_of_str; j++)
-	{
-		end_ptr++;
-	}
-
-	/* Swap the char from start and end */
-	/* index using begin_ptr and end_ptr */
-
-	for (j = 0; j < len_of_str / 2; j++)
-	{
-		/* swap character */
-		ch = *end_ptr;
-		*end_ptr = *begin_ptr;
-		*begin_ptr = ch;
-
-		/* update pointers positions */
-		begin_ptr++;
-		end_ptr--;
-	}
+    while (i <= len)
+    {
+        temp = s[i];
+        s[i] = s[len];
+        s[len] = temp;
+        i++;
+        len--;
+    }
 
 }
-
