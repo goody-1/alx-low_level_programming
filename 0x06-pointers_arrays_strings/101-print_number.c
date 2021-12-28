79e235909bef0eef671f15647b357d0e79eb712e
Goodness <atundeg@gmail.com>
2021-12-28 12:24:55 +0100

    advanced task 1

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_number - prints an integer
+* @n: integer to be printed
+* Return: no return (return is void)
+*/
+
+void print_number(int n)
+{
+	unsigned int c;
+
+	c = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		c = -n;
+	}
+
+	if (c / 10)
+		print_number(c / 10);
+
+	_putchar((c % 10) + '0');
+}
