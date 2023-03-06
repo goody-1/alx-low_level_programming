2b607727e2a7712919ba82f8a7bbff23f036118b
Goodness <atundeg@gmail.com>
2023-03-06 22:51:08 +0100

    rename 0-holberton.c to 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - program that prints Holberton, followed by a new line.
+ *
+ * Return: returns 0
+ *
+ */
+
+int main(void)
+{
+	char *text = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
