ec96cb5ca09f7188878624e3cf7ed5da7ccc64f9
goody-1 <atundeg@gmail.com>
2024-01-13 16:21:16 +0100

    write function for largest prime number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,7 @@
+#include <math.h>
 #include "main.h"
+
+long largest_prime_factor(long num);
 
 /**
  * main - check the code
@@ -7,22 +10,40 @@
  */
 int main(void)
 {
-	long num = 612852475143, temp = num, quotient;
-	int i = 2;
+	long lpf = largest_prime_factor(612852475143);
 
-	while (i < 5000)
-	{
-		if (temp % i == 0)
-		{
-		  /* printf("temp %ld is divisible by %d; ", temp, i); */
-			temp = temp / i;
-		  /* printf("its quotient is %ld\n", temp); */
-			i = 2;
-		}
-		i++;
-	}
-	quotient = temp;
-	printf("%ld\n", quotient);
-
+	printf("%ld\n", lpf);
 	return (0);
+}
+
+/**
+ * largest_prime_factor - print the largest prime factor of a given number
+ *
+ * @num: the number whose largest prime factor is to be found
+ * Return: the largest prime factor
+*/
+long largest_prime_factor(long num)
+{
+	long lpf = 1, temp, i;
+
+	temp = num;
+
+	/* keep dividing even numbers by 2 */
+	while (temp % 2 == 0)
+	{
+		temp /= 2;
+		lpf = temp;
+	}
+
+	/* find the largest prime factor for the number left */
+	for (i = 3; i < sqrt(temp); i += 2)
+	{
+		/* keep dividing if possible */
+		while (temp % i == 0)
+		{
+			temp /= i;
+			lpf = temp;
+		}
+	}
+	return (lpf);
 }
