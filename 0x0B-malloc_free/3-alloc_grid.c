9044e0a9882d10dabdc016be1059d1162e1fe73f
goody-1 <atundeg@gmail.com>
2024-01-16 15:24:44 +0100

    write function to create a grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * alloc_grid -   returns a pointer to a 2 dimensional array of integers.
+ *
+ * @width: width of matrix
+ * @height: height of matrix
+ *
+ * Return: NULL if program fails,,
+ *      NULL if width or height is less than 1,
+ *		pointer to array if successful
+*/
+int **alloc_grid(int width, int height)
+{
+	int **matrix;
+	int *row;
+	int i = 0, j = 0;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	matrix = malloc(sizeof(int) * width + 1);
+
+	while (i < width)
+	{
+		matrix[i] = malloc(sizeof(int) * height + 1);
+		row = matrix[i];
+		while (j < height)
+		{
+			row[j] = 0;
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+
+	return (matrix);
+}
