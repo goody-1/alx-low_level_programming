824cf71e3e1d915e7d405623ba1eedce6e751c00
goody-1 <atundeg@gmail.com>
2024-01-16 17:55:37 +0100

    add prototypes for advanced tasks functions

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -11,4 +11,11 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 
+/* advanced tasks */
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+/* utilities */
+unsigned int str_len(char *str);
+
 #endif
