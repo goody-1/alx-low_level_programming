253cd473c44a7433ee9435b17a84ee7f67897bde
goody-1 <atundeg@gmail.com>
2024-01-17 01:35:20 +0100

    add more comments on understanding the function's logic

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -22,6 +22,8 @@ char **strtow(char *str)
 	/* Count the number of words */
 	for (i = 0; i < str_length; i++)
 	{
+		/* number of words is determined by taking count of the */
+		/* last character in every word */
 		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 			words_num++;
 	}
@@ -46,6 +48,11 @@ char **strtow(char *str)
 			len++;
 			k++;
 		}
+		/**
+		 * for (; k < str_length && str[k] != ' '; k++, len++)
+		 *		;
+		 * The statement above can be used as well
+		 */
 
 		/* Allocate space for the word with null terminator */
 		words[i] = malloc(sizeof(char) * (len + 1));
@@ -61,9 +68,8 @@ char **strtow(char *str)
 
 		/* Copy the characters of the current word */
 		for (j = 0; j < len; j++)
-		{
+			/* pick from beginning of current word */
 			words[i][j] = str[k - len + j];
-		}
 		words[i][len] = '\0'; /* Null-terminate the word */
 	}
 
