ba11262ed8eec2be0203b853bb3720b3e51fe921
goody-1 <atundeg@gmail.com>
2024-01-18 16:04:57 +0100

    test freeing mem

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,8 +11,10 @@ void *malloc_checked(unsigned int b)
 {
 	void *mem = malloc(b);
 
-	if (mem == NULL || b < 1)
+	if (mem == NULL || b >= INT_MAX || b < 1)
+	{
+		free(mem);
 		exit(98);
-
+	}
 	return (mem);
 }
