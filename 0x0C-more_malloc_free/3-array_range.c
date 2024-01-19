a59e5e35aefcca7086368b718a2ef552f7f2ee2a
goody-1 <atundeg@gmail.com>
2024-01-19 07:18:20 +0100

    check for null after malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,9 +15,12 @@ int *array_range(int min, int max)
 	int *array;
 	int size = max - min + 1, i;
 
+	if (min > max)
+		return (NULL);
+
 	array = malloc(sizeof(int) * size);
 
-	if (min > max || array == NULL)
+	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++, min++)
