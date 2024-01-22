1f687abaabf6c04ed69b08e34f01ecc09e177805
goody-1 <atundeg@gmail.com>
2024-01-22 19:38:58 +0100

    add return type to documentation

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,6 +9,7 @@ char *str_cpy(char *dest, char *src);
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
+ * Return: dog_t type
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
