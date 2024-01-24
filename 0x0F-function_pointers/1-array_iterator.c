6af6dd73110df1167ec912b439631b6384db65f1
goody-1 <atundeg@gmail.com>
2024-01-24 01:39:28 +0100

    write function that uses another function over an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator -  executes a function given
+ *		as a parameter on each element of an array
+ *
+ * @array: array to be iterated
+ * @size: size of the array
+ * @action: function pointer to be used
+ *
+ * Return: void
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	void (*func_ptr)(int) = action;
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		func_ptr(array[i]);
+
+}
