bea1819a944695ae902d9d2e002b8cc0362c1726
goody-1 <atundeg@gmail.com>
2024-01-24 17:35:44 +0100

    create struct for printing argument types

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,6 +7,19 @@
 #include <stddef.h>
 #include <stdarg.h>
 
+/**
+ * struct print - Struct print
+ *
+ * @ch: format identifier
+ * @print_type: The function for identifier, i.e int, char etc
+ */
+typedef struct print
+{
+	char ch;
+	void (*print_type)(va_list arg);
+} print_t;
+
+
 /* prototypes for functions */
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
