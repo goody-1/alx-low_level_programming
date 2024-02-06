23428783d4afd73b446a5a66f69cc3f397bc1784
goody-1 <atundeg@gmail.com>
2024-02-06 11:43:25 +0100

    revert to best number of passes

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,18 +9,9 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0, len = 0;
-	listint_t *temp = head;
+	unsigned int i = 0;
 
 	if (!head)
-		return (NULL);
-
-	while (temp)
-	{
-		temp = temp->next;
-		len++;
-	}
-	if (index < 1 || index > len)
 		return (NULL);
 
 	while (i < index)
@@ -28,7 +19,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		head = head->next;
 		i++;
 	}
-	free(temp);
 
 	return (head);
 }
