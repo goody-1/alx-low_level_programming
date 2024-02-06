d9ebac637714b942701b9fd31d705425e40bb399
goody-1 <atundeg@gmail.com>
2024-02-06 16:19:39 +0100

    remove printf statement

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp = temp->next;
 		i++;
 	}
-	printf("Here %d\n", temp->n);
 
 	if (temp == NULL || temp->next == NULL)
 		return (failure);
