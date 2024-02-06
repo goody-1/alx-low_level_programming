0efd96d6a886eb21b1c3f52cb6668a3c5b1dc95c
goody-1 <atundeg@gmail.com>
2024-02-06 13:40:23 +0100

    shorten lines to satisfy betty style

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,18 +11,16 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int counter = 1;
-	listint_t *new_node, *temp;
+	listint_t *new_node = malloc(sizeof(listint_t)), *temp;
 
 	if (!head)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
 
 	temp = *head;
-
 	if (idx == 0)
 	{
 		new_node->next = temp;
@@ -44,7 +42,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp->next = new_node;
 			return (new_node);
 		}
-
 		counter++;
 		temp = temp->next;
 	}
