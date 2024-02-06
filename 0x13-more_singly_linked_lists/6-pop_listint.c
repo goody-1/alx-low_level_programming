4b5890c932f56e61f8a0f058430a59eee1310fd1
goody-1 <atundeg@gmail.com>
2024-02-06 09:27:29 +0100

    free the popped node - valgrind concerns

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,12 +9,15 @@
 int pop_listint(listint_t **head)
 {
 	int data;
+	listint_t *temp;
 
 	if (!head)
 		return (0);
 
 	data = (*head)->n;
+	temp = *head;
 	*head = (*head)->next;
 
+	free(temp);
 	return (data);
 }
