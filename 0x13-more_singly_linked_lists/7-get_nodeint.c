b124981cc4b4ae562b5cba53ec687f001d9e61ed
goody-1 <atundeg@gmail.com>
2024-02-06 12:15:07 +0100

    handle last node reached correctly

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (!head)
 		return (NULL);
 
-	while (i < index)
+	while (i <= index)
 	{
 		if (i == index)
 			return (head);
@@ -22,5 +22,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		i++;
 	}
 
-	return (head);
+	return (NULL);
 }
