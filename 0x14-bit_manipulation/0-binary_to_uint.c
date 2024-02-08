fdaf1bf11f9dfa939dd4ecc84b765231d40be694
goody-1 <atundeg@gmail.com>
2024-02-08 23:05:29 +0100

    write function to convert binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,86 @@
+#include "main.h"
+
+void print_number(int n);
+int positive_power(int n, int p);
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ *
+ * @b: binary string
+ *
+ * Return:  the converted number, or 0 if
+ *				there is at least a character not binary in the string
+ *				b is NULL
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, len, dec = 0;
+	int j;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] !=  '1')
+			return (0);
+	}
+
+	for (len = 0; b[len] != '\0'; len++)
+	;
+
+	j = len - 1;
+	i = 0;
+
+	while (j >= 0 && i < len)
+	{
+		dec += (b[i] - '0') * positive_power(2, j);
+
+		j--;
+		i++;
+	}
+
+	return (dec);
+}
+/**
+* print_number - prints an integer
+* @n: integer to be printed
+* Return: no return (return is void)
+*/
+void print_number(int n)
+{
+	unsigned int c;
+
+	c = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		c = -n;
+	}
+
+	if (c / 10)
+		print_number(c / 10);
+
+	_putchar((c % 10) + '0');
+}
+/**
+* positive_power - gets the power of a number
+* @n: integer
+* @p: index of n
+* Return: power of number
+*/
+int positive_power(int n, int p)
+{
+	int i, pow = n;
+	if (n == 0)
+		return (0);
+	if (p == 0)
+		return (1);
+	if (p < 0)
+		return (-1);
+
+	for (i = 1; i < p; i++)
+	{
+		pow *= n;
+	}
+	return (pow);
+}
