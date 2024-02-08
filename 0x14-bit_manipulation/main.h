a8f237123b2c3588ea9e07c647456db4dc95e999
goody-1 <atundeg@gmail.com>
2024-02-08 23:08:06 +0100

    update header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -13,4 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
+/* Helper functions */
+int _putchar(char c);
+
 #endif
