8687c371bff75080d2e894e59ea2a80ed57de7b1
goody-1 <atundeg@gmail.com>
2024-02-09 12:53:24 +0100

    add prototype for advanced task function

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -13,6 +13,9 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
+/* Advanced Tasks */
+int get_endianness(void);
+
 /* Helper functions */
 int _putchar(char c);
 
